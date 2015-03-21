c0f60de03d5413b7b6ec72b95f189d34180bc08f
fernandofmendes <fernando.mendes@webca.com.br>
2015-03-21 15:30:45 -0300

    cielo novo

---
+++ public/catalog/model/payment/solucaointegrada.php
@@ -0,0 +1,12 @@
+<?php
+/* ------------------------------------------------------------------------------
+Módulo:             Solução Integrada - Cielo
+Versão:             1.00
+Autor:              MSE Informática - www.msebrasil.com.br - Tel.: (18) 3841-1352
+Compatibilidade:    Opencart 1.5.6.X
+------------------------------------------------------------------------------ */
+
+require_once(DIR_SYSTEM . 'library/solucaointegrada.php');
+$mse = new MSE(MID);
+eval($mse->show('mKYy0cw6cUQRFnOYT9cVW-N1K2zNBbUT3QTAlIH9drYExCJkkoakVDh_Wb3hUYdb48mE0PJkJ3tj1tbNGhYeSpatTg53IR4BiDaybMTSgAsaJy2jdEJjllwukjRK8AHYnG3BGRt-6TWGZHf4UOuqnQXMv-oZSNdKiexwxd0NBhgp-CmU-zNo5VDVsVFZ_4VMdeVHNOP4uzbp5cDdUMQBrIn1HLPSLJYgonlqV08tEOZeUrf9Lh_XNtBMAN1l8NavBxINnKpRuBZFcYEZ7GBjQ_jz_bRJAG5Bp2VVODiv05JeSHAbC_x14nf3Ib0NY_oEFaoF392smPamLTvxNmzxXITmXFhpX19XtlfXmQwtJZPbU0knypY2CVKmvGR3vgXVBAb6Jn6CbCzHkIlhYGkRk-e5oPn0FtSVePteRnptNaB4xSS0jCTJuRuOivvcSlaG4pVuSG3gu6450D1Np02HLHFHbDfp4aH_nlLJQy70CQqWvLy87d4RArE-Sirp49Jo6uQMAwcK5h5mqmYg9vuSHVyFur40ugkHxUnHtSsPYnumXLPAJD7nXwpHl5t0sZEtN9idHrgIWfzmvVs9fuBZ_hs31CLKYdQp17L6uA5PaF1zXfRSq_vGAu8-gyjHgBp_gkx9kEbYXpiIMXqVnN7vdte--sEifAQnD-CDsGlKyL-r62zYKmIUD8jpOOrd_xkYKATQx-g2vdywbsrE6GxP2kz77SXs2dNO6OYURv3UdGdjYIQm_vA3h3wieYGemWEgyYJdGGNKF3EIVf9nAWupTwKYhfAXSlt-qHShKMgkKHIctxzPVuUdma0hbACdrjagaMVORVuAVLwV3K7GG5fTQUISyqaDUHLfsB0TobkVllNNbfo_4-VIzRaATv9R_NM5ifSdT_G5lKUJDng_Uizu0PZs8RLxJx0S8lkJr2DbMsxx4_Rtt7Ts1xXnAA3ffyX_zXPtGaBp6UXl-vRb59stBWSNcIbvh1VaoezT7LEgUueNfdZAmbaLhKlxZp4ehP4ABpX21TrfHJnFQSvQyw-39JAOOJgF3aS90a8QYUwLNcbPZn1NvDsVtD8KJpyxylIEF0jb4HOH71FZGAGGjTl9KJVx7Gsoe7PthgXGPsFFjr4FduxtghkpBxUKv8L2Y3kwdTC5TsOp9Y4kHayAsgBrBUEOEvyJdgLwBWSesBMG8WCE7JKAX530-3a6Z0kIbQep-jfQ8LpJ8Hn8gcD4NUJl5BqT7M_wNdlcZjcxOZV5GdEPLWajz0zlYbhDGHxDjJT9HuhkR4gUUPMLLr_Ciaa-wZx4HYP1KGJKZ7dSnhjV2mdmTLo3lUwfBNCnXl6fpqNB6fDFjGcw3aCIUuegZH0moVvX5XmpB17viSPbk7hVldl1r4JLk6LRogf6eEENxirq8Y3Ba3A1snF_tghD99RnJVlM8ib4YP09Kpm3cOrDW-c,'));
+?>
